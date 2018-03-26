eccd40ba73bfab16ceabb417ff3e3f27514271e3
Baki Sahin <bakisahin@yahoo.com>
2018-03-26 10:34:50 +0300

    Create high_value_of_partitions.sql

---
+++ high_value_of_partitions.sql
@@ -0,0 +1,21 @@
+http://www.oracle-developer.net/display.php?id=430 
+
+Here's a quick example borrowed from there showing how to get this in SQL using XML conversion: 
+
+create table t (
+  x date 
+) partition by range (x) (
+  partition p0 values less than (date'2015-01-01'),
+  partition p1 values less than (date'2015-06-01'),
+  partition pmax values less than (maxvalue)
+);
+
+with xml as (
+  select dbms_xmlgen.getxmltype('select table_name, partition_name, high_value from user_tab_partitions where table_name = ''T''') as x
+  from   dual
+)
+  select extractValue(rws.object_value, '/ROW/TABLE_NAME') table_name,
+         extractValue(rws.object_value, '/ROW/PARTITION_NAME') partition,
+         extractValue(rws.object_value, '/ROW/HIGH_VALUE') high_value
+  from   xml x, 
+         table(xmlsequence(extract(x.x, '/ROWSET/ROW'))) rws;
